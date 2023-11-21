92fc7e34debbe39ed74970ec29d4689b1df78eb7
Harsh Mishra <harshm1804@gmail.com>
2023-11-21 22:37:29 +0530

    Commit after Profile Page

--- models.py
+++ models.py
@@ -6,13 +6,15 @@ db = SQLAlchemy(app)
 
 # Models
 class User(db.Model):
-    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
     passhash = db.Column(db.String(512), nullable=False)
     name = db.Column(db.String(64), nullable=False)
     role = db.Column(db.String(20), nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    albums = db.relationship('Album', backref='artist', lazy=True)
+    songs = db.relationship('Song', backref='artist', lazy=True)
+    playlists = db.relationship('Playlist', backref='user', lazy=True)
 
     @property
     def password(self):
@@ -25,33 +27,38 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.passhash, password)
 
-# class Album(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(64), nullable=False)
-#     artist_id = db.Column(db.Integer, nullable=False)
-#     release_date = db.Column(db.Date)
-#
-# class Song(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255), nullable=False)
-#     lyrics = db.Column(db.Text)
-#     duration = db.Column(db.Integer)
-#     date_created = db.Column(db.Date)
-#     album_id = db.Column(db.Integer)
-#
-# class Playlist(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255), nullable=False)
-#     user_id = db.Column(db.Integer)
-#
-# class PlaylistSong(db.Model):
-#     playlist_id = db.Column(db.Integer, primary_key=True)
-#     song_id = db.Column(db.Integer, primary_key=True)
-#
-# class Rating(db.Model):
-#     user_id = db.Column(db.Integer, primary_key=True)
-#     song_id = db.Column(db.Integer, primary_key=True)
-#     rating_value = db.Column(db.Float)
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    release_date = db.Column(db.Date)
+    genre = db.Column(db.String(64))
+    artist_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    songs = db.relationship('Song', backref='album', lazy=True)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    lyrics = db.Column(db.Text)
+    release_date = db.Column(db.Date)
+    genre = db.Column(db.String(64))
+    thumbnail = db.Column(db.String(255))
+    music_src = db.Column(db.String(255))
+    artist_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'), default=None)
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class PlaylistSong(db.Model):
+    playlist_id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, primary_key=True)
+
+class Rating(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, primary_key=True)
+    rating_value = db.Column(db.Float, default=0.0)
 
 with app.app_context():
     db.create_all()
--- routes.py
+++ routes.py
@@ -4,7 +4,10 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    else:
+        return render_template('index.html')
 
 @app.route('/signup', methods = ['POST','GET'])
 def signup():
@@ -17,7 +20,7 @@ def signup():
         role = roles[int(request.form.get('role')) - 1]
 
         if username == '' or name == '' or password == '' or confirm_password == '':
-            flash('Please fill out all fields', 'info')
+            flash('Please fill out all fields', '')
             return redirect(url_for('signup'))
 
         if password != confirm_password:
@@ -51,10 +54,57 @@ def login():
             flash('Incorrect password', 'info')
             return redirect(url_for('login'))
         else:
-            session['user'] = user.username
-            session['role'] = user.role
+            session['user_id'] = user.id
             return redirect(url_for('index'))
     else:
         return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    else:
+        user = User.query.filter_by(id=session['user_id']).first()
+        return render_template('profile.html', user=user)
+
+@app.route('/change_password', methods = ['GET','POST'])
+def change_password():
+    user = User.query.filter_by(id=session['user_id']).first()
+    old_password = request.form.get('old_password')
+
+    if not user.verify_password(old_password):
+        flash('Incorrect password', 'info')
+        return redirect(url_for('profile'))
+
+    new_password = request.form.get('new_password')
+    confirm_password = request.form.get('confirm_password')
+    if new_password != confirm_password:
+        flash('Passwords do not match', 'info')
+        return redirect(url_for('profile'))
+
+    user.password = new_password
+    db.session.commit()
+    flash('Password changed', 'info')
+    return redirect(url_for('profile'))
+
+@app.route('/become_creator', methods=['GET','POST'])
+def become_creator():
+    user = User.query.filter_by(id=session['user_id']).first()
+    user.role = 'Creator'
+    db.session.commit()
+    flash('You are now a creator', 'info')
+    return redirect(url_for('profile'))
+
+@app.route('/uploads')
+def uploads():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    else:
+        user = User.query.filter_by(id=session['user_id']).first()
+        return render_template('uploads.html', user=user)
 
